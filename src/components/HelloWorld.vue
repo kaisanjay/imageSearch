94d141ef71cd5d9d8947f79d9b6bc58e35558f2a
Sanjay Sharma <sanjay.wittypen@gmail.com>
2019-01-04 22:42:21 +0530

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <h2>hello world </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
