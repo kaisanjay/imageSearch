55902e54ed88b604adeddff3291a8eb78e17c494
Sanjay Sharma <sanjay.wittypen@gmail.com>
2019-01-04 23:09:43 +0530

    :heavy_plus_sign: add axios, :sparkles: hook api

---
+++ src/components/landingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+     <div v-for="a in unsplashData">
+         <img :src="a.urls.thumb">
+     </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      unsplashData: []
+    }
+  },
+
+  created () {
+    const clientId = '5f39d0a9e6755434f948e418ab5ce36ae00b133409a4b988c59a633ddbe594ba'
+    axios.get('https://api.unsplash.com/photos/?client_id=' + clientId).then(response => {
+      this.unsplashData = response.data
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import landingPage from '@/components/landingPage'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'landingPage',
+      component: landingPage
     }
   ]
 })
