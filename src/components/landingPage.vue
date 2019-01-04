4d5641c0c989b504073834f8b30c7866ececa006
Sanjay Sharma <sanjay.wittypen@gmail.com>
2019-01-05 00:47:02 +0530

    :truck: update sass file structure, :sparkles: add search bar functionality

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-     <div v-for="a in unsplashData" class="test">
-         <img :src="a.urls.small">
+    <div class="newsletter">
+      <input type="text"
+      v-model="userInput"
+      id="emailInput"
+      @keyup.enter="search"
+      placeholder="Search photos">
+      <button @click="search">
+        Sign Up
+      </button>
+    </div>
+
+     <div v-for="a in unsplashData">
+         <img :src="a.urls.small" class="image-card">
      </div>
   </div>
 </template>
@@ -13,21 +24,31 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      unsplashData: []
+      unsplashData: [],
+      userInput: null,
+      clientId: '5f39d0a9e6755434f948e418ab5ce36ae00b133409a4b988c59a633ddbe594ba'
     }
   },
 
   created () {
-    const clientId = '5f39d0a9e6755434f948e418ab5ce36ae00b133409a4b988c59a633ddbe594ba'
-    axios.get('https://api.unsplash.com/photos/?client_id=' + clientId).then(response => {
+    axios.get('https://api.unsplash.com/photos/?client_id=' + this.clientId).then(response => {
       this.unsplashData = response.data
     })
+  },
+
+  methods: {
+    search () {
+      this.unsplashData = null
+      axios.get('https://api.unsplash.com/photos/search/?query=' + this.userInput + '&client_id=' + this.clientId).then(response => {
+        console.log(response.data)
+        this.unsplashData = response.data
+      })
+    }
   }
 }
 </script>
 
 <style lang="sass">
-
 @import '../styles/app.sass'
 
 
